d1a5e20bd8d8ec1dce2363986b1924067d1bcd80
emir <contact@ekherchi.fr>
2020-03-19 15:33:29 +0100

    elements toggle

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
  
 
-let toKnow = document.getElementById('toKnow');
 
-toKnow.onclick = showInfo;
+    document.getElementById("bro").onclick = function(){
+        document.getElementById("info").classList.toggle("show");
+        style.cssText ="-webkit-transition: all 0.5s ease-in-out;";
+    };
+    
+       const newElement = document.createElement("div");
 
-function showInfo(){
-    alert("Evènement de click détecté"); 
-}
+       let elementMain = document.getElementById("presentation");
+
+
+       elementMain.appendChild(newElement);
+
+       //newElement.innerHTML = "<h1>BIIIIIIIIIIgTUUUURtle</h1>"; 
