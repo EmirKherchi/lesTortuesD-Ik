aa6b604856c4bd4dc37f92ddc39fbc6cfe1d95fb
emir <contact@ekherchi.fr>
2020-07-26 01:22:17 +0200

    ajout modal image et text dynamique et filter background modal avec btn close

--- script.js
+++ script.js
@@ -11,10 +11,25 @@ const pick = document.getElementById("pick");
 const moreInfoBtn = document.getElementsByClassName("more");
 const infosHidden = document.getElementsByClassName("hide-infos");
 
+const main = document.querySelector('main');
+
 const modalDiv = document.getElementById("modal");
 const imgModal = document.getElementById("img_modal");
 const textModal = document.getElementById("text_modal");
+const closeModal = document.getElementById("close");
 modalDiv.classList.add("displayNone");
+
+let allImg = [];
+allImg = document.querySelectorAll("img");
+
+let allsrcImages = [];
+
+for (let k = 0; k < allImg.length; k++) {
+  allsrcImages.push(allImg[k].src);
+}
+allsrcImages.shift(); // supprime la src de la liste des images car pas une src utilisable reste uniquement les bonnes images
+
+console.log(allsrcImages);
 
 function show(display, section) {
   display.addEventListener("click", function () {
@@ -26,17 +41,27 @@ function show(display, section) {
   });
 }
 
+
+
 for (let i = 0; i < moreInfoBtn.length; i++) {
   infosHidden[i].classList.add("displayNone");
 
+
   moreInfoBtn[i].addEventListener("click", function () {
     modalDiv.classList.remove("displayNone");
-    modalDiv.classList.add('modalDisplay');
+    modalDiv.classList.add("modalDisplay");
     for (let j = 0; j < infosHidden.length; j++) {
       textModal.innerHTML = infosHidden[i].textContent;
+      imgModal.src = allsrcImages[i];
+      main.classList.add('filterMain');
     }
   });
 }
+
+closeModal.addEventListener("click", function () {
+    modalDiv.classList.add("displayNone");
+    main.classList.remove('filterMain');
+  });
 
 show(display1, section1);
 show(display2, section2);
